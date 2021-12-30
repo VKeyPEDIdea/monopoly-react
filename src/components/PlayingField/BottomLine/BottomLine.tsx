03b2d7d72ff3b0c784fd114a62eaa236aba245ff
Karyapkin Pavel <pavel.karyapkin@fuse8.online>
2021-12-30 16:25:47 +0600

    add test content

--- src/components/PlayingField/BottomLine/BottomLine.tsx
+++ src/components/PlayingField/BottomLine/BottomLine.tsx
@@ -4,27 +4,26 @@ import classes from './BottomLine.module.scss';
 const BottomLine = () => {
 	return (
 		<div className={classes.bottomline}>
-			<Sector isBottom={true}/>
-			<Sector isBottom={true}/>
-			<Sector isBottom={true}/>
-			<Sector isBottom={true}/>
-			<Sector isBottom={true}/>
-			<Sector isBottom={true}/>
-			<Sector isBottom={true}/>
-			<Sector isBottom={true}/>
-			<Sector isBottom={true}/>
-			<Sector isBottom={true}/>
-			<Sector isBottom={true}/>
-			<Sector isBottom={true}/>
-			<Sector isBottom={true}/>
-			<Sector isBottom={true}/>
-			<Sector isBottom={true}/>
-			<Sector isBottom={true}/>
-			<Sector isBottom={true}/>
-			<Sector isBottom={true}/>
-			<Sector isBottom={true}/>
-			<Sector isBottom={true}/>
-			<Sector isBottom={true}/>
+			<Sector isBottom={true} title='Гостиничный комплекс' price={400}/>
+			<Sector isBottom={true} title='Дорогая покупка' price={0}/>
+			<Sector isBottom={true} title='Курорт зона' price={350}/>
+			<Sector isBottom={true} title='Шанс' price={0}/>
+			<Sector isBottom={true} title='Южный порт' price={0}/>
+			<Sector isBottom={true} title='Авиакомпания' price={320}/>
+			<Sector isBottom={true} title='Банк' price={0}/>
+			<Sector isBottom={true} title='Железная дорога' price={300}/>
+			<Sector isBottom={true} title='Морские перевозки' price={300}/>
+			<Sector isBottom={true} title='Арест' price={0}/>
+			<Sector isBottom={true} title='Сотовая связь' price={280}/>
+			<Sector isBottom={true} title='Водопровродная компания' price={0}/>
+			<Sector isBottom={true} title='Интернет' price={260}/>
+			<Sector isBottom={true} title='Компьютеры' price={260}/>
+			<Sector isBottom={true} title='Восток порт' price={0}/>
+			<Sector isBottom={true} title='Ресторан' price={240}/>
+			<Sector isBottom={true} title='Ночной клуб' price={220}/>
+			<Sector isBottom={true} title='Шанс' price={0}/>
+			<Sector isBottom={true} title='Бар' price={220}/>
+			<Sector isBottom={true} title='бесплатная парковка' price={0}/>
 		</div>
 	);
 };
--- src/components/PlayingField/Sector/Sector.tsx
+++ src/components/PlayingField/Sector/Sector.tsx
@@ -2,10 +2,14 @@ import classes from './Sector.module.scss';
 
 interface SectorProps {
 	isBottom?: boolean;
+	title: string;
+	price: number;
 };
 
 const Sector = ({
 	isBottom,
+	price,
+	title
 }: SectorProps) => {
 	const color = 'orange';
 	
@@ -14,8 +18,8 @@ const Sector = ({
 			<div className={classes.sector}>
 				<div className={`${classes.color} ${classes[`color--${color}`]}`}></div>
 				<div className={classes.meta}>
-					<p className={`${classes.title} ${classes['title--bottom']}`}>Гапеева</p>
-					<p className={`${classes.price} ${classes['price--bottom']}`}>200</p>
+					<p className={`${classes.title} ${classes['title--bottom']}`}>{title}</p>
+					<p className={`${classes.price} ${classes['price--bottom']}`}>{price}</p>
 				</div>
 			</div>
 		);
@@ -24,8 +28,8 @@ const Sector = ({
 	return (
 		<div className={classes.sector}>
 			<div className={classes.meta}>
-				<p className={classes.price}>200</p>
-				<p className={classes.title}>Гапеева</p>
+				<p className={classes.price}>{price}</p>
+				<p className={classes.title}>{title}</p>
 			</div>
 			<div className={`${classes.color} ${classes[`color--${color}`]}`}></div>
 		</div>
