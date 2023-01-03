8c9ac83b73b2105b0897e853b31530cc405946ba
Karyapkin Pavel <pavel.karyapkin@fuse8.online>
2023-01-03 19:07:41 +0600

    add test for getSectorCoordinates

---
+++ src/utilities/getSectorCoordinates/getSectorCoordinates.test.ts
@@ -0,0 +1,10 @@
+import { describe } from '@jest/globals';
+import getSectorCoordinates from './getSectorCoordinates';
+
+describe('function getSectorCoordinates', () => {
+    it('should return the coordinates of the sector', () => {
+        const element: HTMLDivElement = document.createElement('div');
+        const coordinates = getSectorCoordinates(element, 'Top');
+        expect(coordinates).toEqual({ x: 0, y: 0 });
+    });
+});
