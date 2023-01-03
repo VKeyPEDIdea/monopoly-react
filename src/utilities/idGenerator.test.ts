894c349f5ba62c495960277742bf5ec229ca6aa0
Karyapkin Pavel <pavel.karyapkin@fuse8.online>
2023-01-03 18:28:35 +0600

    add unit test for idGenerator

---
+++ src/utilities/idGenerator.test.ts
@@ -0,0 +1,35 @@
+import { describe } from '@jest/globals';
+import { IDGenerator } from './idGenerator';
+
+let idGenerator: IDGenerator | null = null;
+
+describe('class IDGenerator', () => {
+    beforeAll(() => {
+        idGenerator = new IDGenerator();
+    });
+
+    afterAll(() => {
+        idGenerator = null;
+    });
+
+    it('should return a new player ID is 0', () => {
+        if (idGenerator !== null) {
+            const id = idGenerator.getNewPlayerID();
+            expect(id).toBe(0);
+        }
+    });
+
+    it('should return a new sector ID is 0', () => {
+        if (idGenerator !== null) {
+            const id = idGenerator.getNewSectorID();
+            expect(id).toBe(0);
+        }
+    });
+
+    it('should return a new sector ID is 1', () => {
+        if (idGenerator !== null) {
+            const id = idGenerator.getNewSectorID();
+            expect(id).toBe(1);
+        }
+    });
+});
