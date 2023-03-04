6696b54ed0bae4374270e44b1e00f8002bf31762
Pavel Karyapkin <karyapkin@gmail.com>
2023-03-04 22:06:18 +0600

    extract getIndexOfVacant to separate func

--- src/features/field/reducers/buildHouse.ts
+++ src/features/field/reducers/buildHouse.ts
@@ -1,6 +1,7 @@
 import { AppDispatch, RootState } from 'app/store';
 import { Sector } from 'core/Sector/Sector.interface';
 import { decreasePlayersCashCount } from 'features/players/playersSlice';
+import { HousePointProps } from 'shared/ui/HousePoint/HousePoint.model';
 import { setHouseState, setLastUpgradedSector } from '../playingFieldSlice';
 
 const buildHouse = (sectorId: number) => (dispatch: AppDispatch, getState: () => RootState) => {
@@ -17,26 +18,12 @@ const buildHouse = (sectorId: number) => (dispatch: AppDispatch, getState: () =>
 
         if (!lastUpgradedSector) {
             const firstSectorHouseList = sectorList[0].houseList!;
-
-            for (let i = 0; i < firstSectorHouseList.length; i++) {                
-                if (firstSectorHouseList[i].state === 'vacant') {
-                    houseIndex = i;
-                    break;
-                }
-            }
-
+            houseIndex = getIndexOfVacantHouse(firstSectorHouseList);
             sectorId = sectorList[0].id;
         } else {
             const indexOfLastUpgraded = sectorList.indexOf(lastUpgradedSector);
             const nextSector = sectorList[indexOfLastUpgraded + 1] ? sectorList[indexOfLastUpgraded + 1] : sectorList[0];
-
-            for (let i = 0; i < nextSector.houseList!.length; i++) {
-                if (nextSector.houseList![i].state === 'vacant') {
-                    houseIndex = i;
-                    break;
-                }
-            }
-
+            houseIndex = getIndexOfVacantHouse(nextSector.houseList!);
             sectorId = nextSector.id;
             // todo: add logic for updating housePrice in sector
         }
@@ -55,5 +42,13 @@ const buildHouse = (sectorId: number) => (dispatch: AppDispatch, getState: () =>
         }));
     }
 };
+
+function getIndexOfVacantHouse(houseList: HousePointProps[]) {
+    for (let i = 0; i < houseList.length; i++) {
+        if (houseList[i].state === 'vacant') {
+            return i;
+        }
+    }
+}
 
 export default buildHouse;
